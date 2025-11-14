a6c53969b6cf1233e30b42e92b942912a43873e2
karin1212 <206808542+karin1212@users.noreply.github.com>
2025-11-14 12:11:58 +0900

    リソースの取得(レコード単体)まで

--- server.js
+++ server.js
@@ -9,12 +9,41 @@ const kv = await Deno.openKv();
 
 /***  リソースの作成 ***/
 app.post('/api/pokemons', async (c) => {
-  return c.json({ path: c.req.path });
+  //リクエストボディを取得
+  const body =await c.req.parseBody();
+  const record=JSON.parse(body['record']);
+
+  //IDと生成時刻を生成してレコードに追加
+  const id=await getNextId();
+  record['id']=id;
+  record['creatdAt']=new Date().toISOString();
+
+  //リソースの作成
+  await kv.set(['pokemons',id],record);
+
+  //レスポンスの作成
+  c.status(201);
+  c.header('Location',`/api/pokemons/${id}`);
+
+  return c.json({ record });
 });
 
 /*** リソースの取得（レコード単体） ***/
 app.get('/api/pokemons/:id', async (c) => {
-  return c.json({ path: c.req.path });
+  //パラメーターの取得と検証
+  const id=Number(c.req.param('id'));
+
+  //リソース(レコード)の取得
+  const pkmn=await kv.get(['pokemons',id]);
+
+  //レコードがあったとき
+  if(pkmn.value){
+    return c.json(pkmn.value);
+  }
+  else{
+    c.status(404); //404 Not Found
+    return c.json({message:`IDが${id}のポケモンはいませんでした。`});
+  } 
 });
 
 /*** リソースの取得（コレクション） ***/
